59d3bbad4a2a4a5e4cc9b1539a4627183cfe18fe
markossify95 <markoss95@hotmail.com>
2016-04-05 22:20:53 +0200

    Created TwitterPorukaTest class...tests are not implemented yet

---
+++ Twitter/src/com/twitter/testUnits/TwitterPorukaTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.twitter.testUnits;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Marko Kostadinovic	
+ * this class is used for testing of all cases
+ * related to methods from TwitterPoruka class
+ *
+ */
+public class TwitterPorukaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#getKorisnik()}.
+	 */
+	@Test
+	public void testGetKorisnik() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#setKorisnik(java.lang.String)}.
+	 */
+	@Test
+	public void testSetKorisnik() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#getPoruka()}.
+	 */
+	@Test
+	public void testGetPoruka() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#setPoruka(java.lang.String)}.
+	 */
+	@Test
+	public void testSetPoruka() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
